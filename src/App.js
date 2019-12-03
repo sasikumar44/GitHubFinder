1019dff5228eafcc2140dad6f4968e5f8caca923
sasikumar44 <srikumarans9@gmail.com>
2019-12-03 17:11:15 +0530

    2nd day

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
+import axios from 'axios';
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <Users />
+        <div className='container'>
+          <Search />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
---
+++ src/components/users/Search.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <form className='form'>
+          <input type='text' name='text' placeholder='Search Users...' />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
