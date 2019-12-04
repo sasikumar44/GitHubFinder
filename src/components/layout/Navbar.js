c7359b2e9cd4cb33c1d98f8e3eaec646633a57db
sasikumar44 <srikumarans9@gmail.com>
2019-12-04 17:12:58 +0530

    3rd day

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
     <nav className='navbar bg-primary'>
       <h1>
         <i className={icon} />
-
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About us</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
