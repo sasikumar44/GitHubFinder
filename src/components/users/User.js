2fabf80d0568bd627cf82ac457a759a79d5d2ea4
sasikumar44 <srikumarans9@gmail.com>
2019-12-06 18:07:36 +0530

    for home

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
   componentDidMount() {
@@ -26,14 +27,49 @@ export class User extends Component {
       following,
       public_repos,
       public_gists,
-      hierable
+      hireable
     } = this.props.user;
 
     const { loading } = this.props;
 
     if (loading) return <Spinner />;
 
-    return <div> {name}</div>;
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back To Search
+        </Link>
+        Hireable: {''}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location :{location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
